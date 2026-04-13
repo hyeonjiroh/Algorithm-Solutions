5f718022437849065a93391d0945446b5e623e1b
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2026-04-13 17:47:24 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,15 +1,10 @@
 function solution(num) {    
-    for (let i = 0; i < 500; i++) {
-        if (num === 1) {
-            return i;
-        }
-        
-        if (num % 2 === 0) {
-            num /= 2;
-        } else {
-            num = num * 3 + 1;
-        }
+    let count = 0;
+    
+    while (num !== 1 && count < 500) {
+        num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+        count++;
     }
     
-    return -1;
+    return num === 1 ? count : -1;
 }
