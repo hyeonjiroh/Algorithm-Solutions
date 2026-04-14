e60341149621e51fa28a6eb623be6c51f2da740e
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2026-04-14 19:32:53 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.58 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -2,11 +2,7 @@ function solution(n) {
     let answer = '';
     
     for (let i = 1; i <= n; i++) {
-        if (i % 2 !== 0) {
-            answer += "수";
-        } else {
-            answer += "박";
-        }
+        answer += i % 2 !== 0 ? "수" : "박";
     }
     
     return answer;
