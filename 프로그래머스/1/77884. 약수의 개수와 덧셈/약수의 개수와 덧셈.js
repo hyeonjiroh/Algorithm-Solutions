daf79010b4779eff5b1a278318aaf858767437cd
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2026-04-15 17:41:39 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,14 @@
+function solution(left, right) {
+    let sum = 0;
+    
+    for (let i = left; i < right + 1; i++) {
+        // 완전제곱수의 경우 약수의 개수가 홀수
+        if (Number.isInteger(Math.sqrt(i))) {
+            sum -= i;
+        } else {
+            sum += i;
+        }
+    }
+    
+    return sum;
+}
