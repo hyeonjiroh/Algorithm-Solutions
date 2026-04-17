542365797e816c8eff482fe36ef1da21b1ad9506
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2026-04-17 21:52:47 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.15 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,13 +1,3 @@
 function solution(arr1, arr2) {
-    let answer = [];
-    
-    for(let i = 0; i < arr1.length; i++) {
-        answer[i] = [];
-        
-        for(let j = 0; j < arr1[i].length; j++) {
-            answer[i][j] = arr1[i][j] + arr2[i][j];
-        }
-    }
-    
-    return answer;
+    return arr1.map((row, i) => row.map((n, j) => n + arr2[i][j]));
 }
