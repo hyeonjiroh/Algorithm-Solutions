b1573d987c159155bf64d668a2e366898f9673cb
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2026-04-18 23:55:02 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,16 +1,9 @@
-// 최대공약수: 유클리드 호제법으로 구하기
-function gcd(a, b) {
-    return b === 0 ? a : gcd(b, a % b);
-}
-
-// 최소공배수: 두 수를 곱한 값을 최대공약수로 나눠서 구하기
-function lcm(a, b) {
-    return (a * b) / gcd(a, b);
-}
-
-function solution(n, m) {
-    let a = Math.max(n, m);
-    let b = Math.min(n, m);
+function solution(n, m) {   
+    // 최대공약수: 유클리드 호제법으로 구하기
+    const gcd = (a, b) => b === 0 ? a : gcd(b, a % b);
     
-    return [gcd(a, b), lcm(a, b)];
+    // 최소공배수: 두 수를 곱한 값을 최대공약수로 나눠서 구하기
+    const lcm = (a, b) => (a * b) / gcd(a, b);
+    
+    return [gcd(n, m), lcm(n, m)];
 }
