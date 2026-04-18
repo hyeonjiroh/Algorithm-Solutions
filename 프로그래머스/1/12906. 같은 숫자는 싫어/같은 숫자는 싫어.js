bb5e14b5ad082e528dbfa993a1f78a922d09d9cc
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2026-04-18 23:34:13 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 47.99 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,11 +1,3 @@
-function solution(arr) {
-    let answer = [arr[0]];
-
-    for (let i = 1; i < arr.length; i++) {
-        if(arr[i] !== arr[i - 1]) {
-            answer.push(arr[i]);
-        }
-    }
-    
-    return answer;
+function solution(arr) {    
+    return arr.filter((n, i) => n !== arr[i - 1]);
 }
