1c42fe1c5a5f66c14f509525874332606f75a9b4
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2026-04-23 22:45:38 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,7 +1,3 @@
 function solution(n) {
-    // n을 3진법으로 변환 후 앞뒤 반전
-    const ternary = n.toString(3).split("").reverse().join("");
-    
-    // 다시 10진법으로 변환
-    return parseInt(ternary, 3);
+     return parseInt(n.toString(3).split("").reverse().join(""), 3);
 }
