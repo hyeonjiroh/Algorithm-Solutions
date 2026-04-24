5757f82c6ccd2911ea1d372f01acb2c0352104bc
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2026-04-24 22:38:33 +0900

    [level 1] Title: 최소직사각형, Time: 4.27 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,14 +1,10 @@
 function solution(sizes) {
     // 각 명함의 긴 쪽을 가로(0번 인덱스)로 통일
-    for (size of sizes) {
-        if (size[0] < size[1]) {
-            [size[0], size[1]] = [size[1], size[0]];
-        }
-    }
+    const normalized = sizes.map(([w, h]) => w < h ? [h, w] : [w, h]);
     
     // 모든 명함을 수납할 수 있는 최소 지갑 크기 반환
-    let maxWidth = Math.max(...sizes.map(a => a[0]));
-    let maxHeight = Math.max(...sizes.map(a => a[1]));
+    let maxW = Math.max(...normalized.map(s => s[0]));
+    let maxH = Math.max(...normalized.map(s => s[1]));
     
-    return maxWidth * maxHeight;
+    return maxW * maxH;
 }
