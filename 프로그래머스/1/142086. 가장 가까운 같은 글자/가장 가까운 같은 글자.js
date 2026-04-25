ede9d1423c11474617244576d99a472e21a69a8f
Hyeonji Roh <108173863+hyeonjiroh@users.noreply.github.com>
2026-04-25 18:23:27 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 17.90 ms, Memory: 37.5 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,16 +1,6 @@
 function solution(s) {
-    let answer = [];
-    
-    for (let i = 0; i < s.length; i++) {
-        // 현재 위치 이전 문자열
-        let prev = s.slice(0, i);
-        
-        // 이전 문자열에서 같은 글자의 마지막 인덱스
-        let lastIndex = prev.lastIndexOf(s[i]);
-        
-        // 같은 글자가 없으면 -1, 있으면 거리 계산
-        answer.push(lastIndex === -1 ? -1 : i - lastIndex);
-    }
-    
-    return answer;
+    return [...s].map((char, i) => {
+        const count = s.slice(0, i).lastIndexOf(char);
+        return count < 0 ? count : i - count;
+    });
 }
